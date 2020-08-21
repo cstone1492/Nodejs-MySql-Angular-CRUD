55db6804958173fd2ad23c68bf8a996f90b97156
Caroline STone <docphilstone@gmail.com>
2020-08-21 16:49:36 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+//builds rest apis
+const bodyParser = require("body-parser");
+//helps to parse the request and create the req.body object
+const cors = require("cors");
+//provides Express middleware to enable CORS with various options
+
+
+//create Express app
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+//add cors middleware
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db = require("./app/models");
+db.sequelize.sync();
+// drop the table if it already exists
+ db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+ });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Caroline's application." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+//for dropping existing tables and re-syncing the database
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
