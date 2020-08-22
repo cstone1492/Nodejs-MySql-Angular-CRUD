89059fe10ce4a8893328c9631c26758201d88315
Caroline STone <docphilstone@gmail.com>
2020-08-22 14:39:03 -0500

    created controllers for dieSEt

--- server/controllers/dieSet.controller.js
+++ server/controllers/dieSet.controller.js
@@ -1,8 +1,8 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const DieSet = db.dieSet;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
+// Create and Save a new dieSet
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
@@ -12,135 +12,136 @@ exports.create = (req, res) => {
     return;
   }
 
-  // Create a Tutorial
+  // Create a dieSet
   const tutorial = {
     title: req.body.title,
     description: req.body.description,
-    published: req.body.published ? req.body.published : false
+    published: req.body.published ? req.body.published : false,
+    includedDice: req.body.includedDice
   };
 
-  // Save Tutorial in the database
-  Tutorial.create(tutorial)
+  // Save dieSet in the database
+  DieSet.create(dieSet)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the dieSet."
       });
     });
 };
 
-// Retrieve all Tutorials from the database.
+// Retrieve all dieSets from the database.
 exports.findAll = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
-  Tutorial.findAll({ where: condition })
+  DieSet.findAll({ where: condition })
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving dieSets."
       });
     });
 };
 
-// Find a single Tutorial with an id
+// Find a single dieSet with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.findByPk(id)
+  DieSet.findByPk(id)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error retrieving Tutorial with id=" + id
+        message: "Error retrieving dieSet with id=" + id
       });
     });
 };
 
-// Update a Tutorial by the id in the request
+// Update a dieSet by the id in the request
 exports.update = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.update(req.body, {
+  DieSet.update(req.body, {
     where: { id: id }
   })
     .then(num => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was updated successfully."
+          message: "DieSet was updated successfully."
         });
       } else {
         res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+          message: `Cannot update dieSet with id=${id}. Maybe dieSet was not found or req.body is empty!`
         });
       }
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating dieSet with id=" + id
       });
     });
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a dieSet with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.destroy({
+  DieSet.destroy({
     where: { id: id }
   })
     .then(num => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: "DieSet was deleted successfully!"
         });
       } else {
         res.send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete dieSet with id=${id}. Maybe dieSet was not found!`
         });
       }
     })
     .catch(err => {
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete dieSet with id=" + id
       });
     });
 };
 
-// Delete all Tutorials from the database.
+// Delete all dieSets from the database.
 exports.deleteAll = (req, res) => {
-  Tutorial.destroy({
+  DieSet.destroy({
     where: {},
     truncate: false
   })
     .then(nums => {
-      res.send({ message: `${nums} Tutorials were deleted successfully!` });
+      res.send({ message: `${nums} dieSets were deleted successfully!` });
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all tutorials."
+          err.message || "Some error occurred while removing all dieSets."
       });
     });
 };
 
-// find all published Tutorial
+// find all published dieSet
 exports.findAllPublished = (req, res) => {
-  Tutorial.findAll({ where: { published: true } })
+  DieSet.findAll({ where: { published: true } })
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving dieSets."
       });
     });
 };
